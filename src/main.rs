9e574586e745a5bb6c1e7a72d80697943dea54b4
liqiang <1484190946@qq.com>
2025-08-27 14:45:39 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use std::fs;
+
+use chrono::{Duration, Utc};
+use clap::Parser;
+
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation, decode, encode};
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser)]
+enum Cli {
+    /// Generate a JWT
+    Gen,
+    /// Verify a JWT
+    Verify,
+}
+
+fn main() {
+    match Cli::parse() {
+        Cli::Gen => generate_jwt(),
+        Cli::Verify => verify_jwt(),
+    }
+    // println!("Hello, world!");
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Cliams {
+    sub: String,
+    exp: i64,
+    iss: String,
+}
+
+fn generate_jwt() {
+    println!("Generating...");
+    let exp = (Utc::now() + Duration::days(1)).timestamp();
+    let token = encode(
+        &Header::new(Algorithm::HS256),
+        &Cliams {
+            sub: "test".to_string(),
+            exp,
+            iss: "test".to_string(),
+        },
+        &EncodingKey::from_secret("secret".as_ref()),
+    )
+    .unwrap();
+    fs::write("token.txt", token).unwrap();
+    println!("Done!");
+}
+
+fn verify_jwt() {
+    println!("Verifying...");
+    let token_string = fs::read_to_string("token.txt").unwrap();
+    let token = decode::<Cliams>(
+        &token_string,
+        &DecodingKey::from_secret("secret".as_ref()),
+        &Validation::default(),
+    )
+    .unwrap();
+    println!("{:?}", token.claims);
+    println!("Done!");
+}
